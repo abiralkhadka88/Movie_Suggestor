9f8f72ac579b32cae04bf0f923573c5f6f7f2e2f
Abiral88 <abiralkhadka88@gmail.com>
2023-06-25 20:54:05 +0545

    Api call and handling requests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+
+  const [movies,setMovies] =  useState([]);
+
+  const [isError,SetisError] = useState(false);  
+
+  const  fetchMovies = async ()=>{
+
+    
+      //fetch resourse
+      console.log('Fetching api');
+
+    try{const response = await axios.get("https://api.dynoacademy.com/test-api/v1/movies");
+    setMovies(response.data.moviesData);}
+    catch(error){
+      SetisError(true);
+    }
+      
+
+      
+      
+
+      
+
+      
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <button onClick={fetchMovies} >Get All Movies</button> <br/>
+
+    {isError?<div style={{background:"red",color:"white",padding:'10px',margin:"10px"}}>
+        Error!
+      </div>
+      :<>
+      <div style={{background:"#e7e7e7",padding:"10px",margin:"7px"}  }>
+
+{ movies.map((el)=>
+   
+ <div key={el.id}> {el.name}  </div>
+ 
+ )}
+ 
+ 
+ 
+</div>
+      </>}
+      
+      
+    
+    
+
     </div>
   );
 }
